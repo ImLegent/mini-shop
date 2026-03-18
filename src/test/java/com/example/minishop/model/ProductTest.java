95ffa52b5de252894cddf28757acc493855ff4ef
ImLegent <z.gent2003@gmail.com>
2026-03-18 20:11:58 +0100

    Run 2 Increase Code Smells

--- src/test/java/com/example/minishop/model/ProductTest.java
+++ src/test/java/com/example/minishop/model/ProductTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ProductTest {
 
